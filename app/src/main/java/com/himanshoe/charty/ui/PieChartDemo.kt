24d23a21e345d1740281fe9ee63c759a7dd40740
Himanshu Singh <hello2himanshusingh@gmail.com>
2022-09-13 21:11:39 +0200

    Redesign pie chart (#36)
    * [Redesign] Pie Chart

--- app/src/main/java/com/himanshoe/charty/ui/PieChartDemo.kt
+++ app/src/main/java/com/himanshoe/charty/ui/PieChartDemo.kt
@@ -1,15 +1,13 @@
 package com.himanshoe.charty.ui
 
+import android.util.Log
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.scale
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -25,26 +23,20 @@ fun PieChartDemo() {
     ) {
 
         val pieData = listOf(
-            PieData(20F),
-            PieData(50F),
-            PieData(100F),
-            PieData(70F),
-            PieData(20F),
-            PieData(50F),
-            PieData(100F),
-            PieData(20F)
+            PieData(3F),
+            PieData(1F),
+            PieData(1F),
         )
-        val pieDataWithCustomColors =
-            listOf(PieData(20F, Color(0xFFfafa6e)), PieData(50F, Color(0xFFc4ec74)))
 
         item {
             PieChart(
                 modifier = Modifier
-                    .scale(1f)
-                    .size(400.dp),
+                    .fillMaxSize(),
                 pieData = pieData,
                 config = PieConfig(isDonut = true, expandDonutOnClick = true),
                 onSectionClicked = { percent, value ->
+                    Log.d("DSfdsfdsf", percent.toString())
+                    Log.d("DSfdsfdsf", value.toString())
                 }
             )
         }
@@ -58,35 +50,12 @@ fun PieChartDemo() {
                 textAlign = TextAlign.Center
             )
         }
-
         item {
             PieChart(
                 modifier = Modifier
-                    .scale(1f)
-                    .size(400.dp),
-                pieData = pieDataWithCustomColors,
-                config = PieConfig(isDonut = true, expandDonutOnClick = false),
-                onSectionClicked = { percent, value ->
-                }
-            )
-        }
-        item {
-            Text(
-                text = "Donut Chart custom colors, no expand",
-                fontSize = 16.sp,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(vertical = 16.dp),
-                textAlign = TextAlign.Center
-            )
-        }
-        item {
-            PieChart(
-                modifier = Modifier
-                    .scale(1f)
-                    .size(400.dp),
+                    .fillMaxSize(),
                 pieData = pieData,
-                config = PieConfig(false),
+                config = PieConfig(isDonut = false, expandDonutOnClick = true),
                 onSectionClicked = { percent, value ->
                 }
             )
