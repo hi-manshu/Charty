e19f52fe26c74dc59405e2ad1f554b72885e11d2
Alex Zhukovich <zhukovich.a@gmail.com>
2022-10-29 14:17:05 +0200

    Fix issue related to not updating max value of lineData

--- app/src/main/java/com/himanshoe/charty/ui/LineChartDemo.kt
+++ app/src/main/java/com/himanshoe/charty/ui/LineChartDemo.kt
@@ -1,12 +1,13 @@
 package com.himanshoe.charty.ui
 
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -14,14 +15,57 @@ import androidx.compose.ui.unit.sp
 import com.himanshoe.charty.line.LineChart
 import com.himanshoe.charty.line.config.LineConfig
 import com.himanshoe.charty.line.model.LineData
+import kotlin.random.Random
 
 @Composable
 fun LineChartDemo() {
+    val dynamicData = remember {
+        mutableStateListOf(
+            LineData(10F, 35F),
+            LineData(10F, 45F),
+        )
+    }
+    
     LazyColumn(
         modifier = Modifier
             .fillMaxSize()
             .padding(32.dp)
     ) {
+        item {
+
+
+            Column {
+                Button(
+                    modifier = Modifier.align(Alignment.CenterHorizontally),
+                    onClick = {
+                        val lastItem = dynamicData.last()
+                        dynamicData.add(
+                            LineData((lastItem.xValue as Float) + 10f, lastItem.yValue + Random.nextFloat() * 10f)
+                        )
+                    }
+                ) {
+                    Text(text = "Add random item")
+                }
+                LineChart(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(300.dp),
+                    colors = colors,
+                    lineData = dynamicData
+                )
+            }
+        }
+        item {
+            Text(
+                text = "Line Chart with dynamic data",
+                fontSize = 16.sp,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 32.dp),
+                textAlign = TextAlign.Center
+            )
+        }
+
         item {
             LineChart(
                 modifier = Modifier
