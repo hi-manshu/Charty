5360a51f04200d4246c88a19b24df1ecc1ab1f0a
yingding <yingdingwang@yahoo.de>
2023-01-14 21:19:02 +0100

    refactor the BarChart with DrawScope.barChart and DrawScope.barChartBackground

--- charty/src/main/java/com/himanshoe/charty/bar/BarChart.kt
+++ charty/src/main/java/com/himanshoe/charty/bar/BarChart.kt
@@ -15,6 +15,8 @@ import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.DrawScope
+import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
 import androidx.compose.ui.input.pointer.pointerInput
 import com.himanshoe.charty.bar.common.calculations.getTopLeft
 import com.himanshoe.charty.bar.common.calculations.getTopRight
@@ -61,51 +63,84 @@ fun BarChart(
     barConfig: BarConfig = BarConfigDefaults.barConfigDimesDefaults()
 ) {
     val maxYValueState = rememberSaveable { mutableStateOf(barData.maxYValue()) }
-    val clickedBar = remember { mutableStateOf(Offset(-10F, -10F)) }
+    val clickedBarState = remember { mutableStateOf(Offset(-10F, -10F)) }
+    val barWidthState = remember { mutableStateOf(0F) }
 
-    val maxYValue = maxYValueState.value
-    val barWidth = remember { mutableStateOf(0F) }
-
-    Canvas(
-        modifier = modifier
+    Canvas(modifier = modifier
+            // draw axis in background
             .drawBehind {
                 if (axisConfig.showAxis) {
-                    drawYAxisWithLabels(
+                    barChartBackground(
                         axisConfig = axisConfig,
-                        maxValue = maxYValue,
-                        textColor = axisConfig.textColor
+                        maxYValue = maxYValueState.value,
                     )
                 }
             }
+            // add padding to canvas
             .padding(horizontal = chartDimens.padding)
             .pointerInput(Unit) {
                 detectTapGestures(onPress = { offset ->
-                    clickedBar.value = offset
+                    clickedBarState.value = offset
                 })
             }
     ) {
-        barWidth.value = size.width.div(barData.count().times(1.2F))
-        val yScalableFactor = size.height.div(maxYValue)
+        barWidthState.value =
+            barChart(
+                barData = barData,
+                colors = colors,
+                onBarClick = onBarClick,
+                axisConfig = axisConfig,
+                barConfig = barConfig,
+                clickedBar = clickedBarState.value,
+                maxYValue = maxYValueState.value
+            )
+    }
+}
+
+fun DrawScope.barChartBackground(
+    axisConfig: AxisConfig,
+    maxYValue: Float,
+) {
+    drawYAxisWithLabels(
+        axisConfig = axisConfig,
+        maxValue = maxYValue,
+        textColor = axisConfig.textColor
+    )
+}
+
+fun DrawScope.barChart(
+    barData: List<BarData>,
+    colors: List<Color>,
+    onBarClick: (BarData) -> Unit,
+    axisConfig: AxisConfig,
+    barConfig: BarConfig,
+    clickedBar: Offset,
+    maxYValue: Float,
+) : Float {
+    val barWidth = size.width.div(barData.count().times(1.2F))
+    val yScalableFactor = size.height.div(maxYValue)
+
+    drawIntoCanvas {
 
         barData.forEachIndexed { index, data ->
-            val topLeft = getTopLeft(index, barWidth.value, size, data.yValue, yScalableFactor)
-            val topRight = getTopRight(index, barWidth.value, size, data.yValue, yScalableFactor)
+            val topLeft = getTopLeft(index, barWidth, size, data.yValue, yScalableFactor)
+            val topRight = getTopRight(index, barWidth, size, data.yValue, yScalableFactor)
             val barHeight = data.yValue.times(yScalableFactor)
 
-            if (clickedBar.value.x in (topLeft.x..topRight.x)) {
+            if (clickedBar.x in (topLeft.x..topRight.x)) {
                 onBarClick(data)
             }
             drawRoundRect(
                 cornerRadius = CornerRadius(if (barConfig.hasRoundedCorner) barHeight else 0F),
                 topLeft = topLeft,
                 brush = Brush.linearGradient(colors),
-                size = Size(barWidth.value, barHeight)
+                size = Size(barWidth, barHeight)
             )
 
             if (axisConfig.showXLabels) {
                 drawBarLabel(
                     data.xValue,
-                    barWidth.value,
+                    barWidth,
                     barHeight,
                     topLeft,
                     barData.count(),
@@ -114,4 +149,5 @@ fun BarChart(
             }
         }
     }
+    return barWidth
 }
