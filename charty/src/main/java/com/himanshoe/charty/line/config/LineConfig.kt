050fa400cb41716195fe01f16f3b5325e674972b
Alberto Caliman <albertocaliman@gmail.com>
2023-01-19 09:06:00 +0100

    added stroke size field on line config (#61)
    Co-authored-by: Alberto Caliman <alberto.caliman@texa.com>

--- charty/src/main/java/com/himanshoe/charty/line/config/LineConfig.kt
+++ charty/src/main/java/com/himanshoe/charty/line/config/LineConfig.kt
@@ -1,14 +1,19 @@
 package com.himanshoe.charty.line.config
 
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
 data class LineConfig(
     val hasSmoothCurve: Boolean = false,
-    val hasDotMarker: Boolean = false
+    val hasDotMarker: Boolean = false,
+    val strokeSize: Dp = 5.dp
 )
 
 internal object LineConfigDefaults {
 
     fun lineConfigDefaults() = LineConfig(
         hasSmoothCurve = true,
-        hasDotMarker = true
+        hasDotMarker = true,
+        strokeSize = 5.dp
     )
 }
